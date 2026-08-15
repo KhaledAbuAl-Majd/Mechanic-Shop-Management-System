c9ea0c4355ad1028942cdad7b80b1a3b69ba6706
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-15 09:17:10 +0300

    feat(Domain-RepairTasks): Create RepairTask Entity, RepiarTaskErros and RepairTaskConstant

---
+++ src/MechanicShop.Domain/RepairTasks/RepairTaskErrors.cs
@@ -0,0 +1,23 @@
+﻿using MechanicShop.Domain.Common.Results;
+
+namespace MechanicShop.Domain.RepairTasks
+{
+    public static class RepairTaskErrors
+    {
+        public static readonly Error IdRequired =
+            Error.Validation("RepairTask.Id.Required", "RepairTask Id is required.");
+
+        public static readonly Error NameRequired =
+        Error.Validation("RepairTask.Name.Required", "Name is required.");
+
+        public static readonly Error LaborCostInvalid =
+            Error.Validation("RepairTask.LaborCost.Invalid",
+                             $"Labor cost must be between {RepairTaskConstant.MinLaborCost} and {RepairTaskConstant.MaxLaborCost:N0}.");
+
+        public static readonly Error DurationInvalid =
+            Error.Validation("RepairTask.Duration.Invalid", "Invalid duration selected.");
+
+        public static Error PartsRequired =
+       Error.Validation("RepairTask.Parts.Required", "At least one part is required.");
+    }
+}
