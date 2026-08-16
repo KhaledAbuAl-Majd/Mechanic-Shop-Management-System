fb07f0160422e8e455ab1a8298de6395789d01e7
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-16 20:32:08 +0300

    feat(Application-DependencyInjection): Register Validators and Mediatr

--- src/MechanicShop.Application/DependencyInjection.cs
+++ src/MechanicShop.Application/DependencyInjection.cs
@@ -1,9 +1,23 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using FluentValidation;
+using MechanicShop.Application.Common.Behaviours;
+
+namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddOpenBehavior(typeof(UnhandledExceptionBehaviour<,>));
+            cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+            cfg.AddOpenBehavior(typeof(PerformanceBehaviour<,>));
+            cfg.AddOpenBehavior(typeof(CachingBehviour<,>));
+        });
 
         return services;
     }
