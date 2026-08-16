c21610d589927204cf9e9663706f54dedd887afa
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-16 12:00:35 +0300

    feat(Application-Identity): Create IIdentityService interface, Identiyt DTO at Features.Identity and Creating IUser -it has current user id-

---
+++ src/MechanicShop.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using MechanicShop.Application.Features.Identity.Dtos;
+using MechanicShop.Domain.Common.Results;
+
+namespace MechanicShop.Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<bool> IsInRoleAsync(string userId, string role);
+        Task<bool> AuthorizeAsync(string userId, string? policyName);
+        Task<Result<AppUserDto>> AuthenticateAsync(string email, string password);//login 
+        Task<Result<AppUserDto>> GetUserByIdAsync(string userId);
+        Task<string?> GetUserNameAsync(string userId);
+    }
+}
