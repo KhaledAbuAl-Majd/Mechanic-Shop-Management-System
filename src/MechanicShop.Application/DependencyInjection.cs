a2f58605f2f5e73bcb5f053fc52945512b6ef921
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-16 10:18:58 +0300

    feat(Application): Add Packages and Creating DependencyInjection class

---
+++ src/MechanicShop.Application/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
