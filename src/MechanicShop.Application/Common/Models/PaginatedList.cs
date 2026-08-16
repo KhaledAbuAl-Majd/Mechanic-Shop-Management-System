f6cb93be6d672fa08c880e721a5400c9e33ddf29
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-16 10:42:36 +0300

    feat(Application-Common-Models): Craete PaginatedList<T> record

---
+++ src/MechanicShop.Application/Common/Models/PaginatedList.cs
@@ -0,0 +1,9 @@
+﻿namespace MechanicShop.Application.Common.Models
+{
+    public record PaginatedList<T>(
+        int PageNumber,
+        int PageSize,
+        int TotalPages,
+        int TotalCount,
+        IReadOnlyCollection<T>? Items);
+}
