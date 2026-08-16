c870f33886598f7077eec21b128018b870b4bddf
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-16 11:35:31 +0300

    feat(Application-Common-Interfaces): Create ICachedQuery

---
+++ src/MechanicShop.Application/Common/Interfaces/ICachedQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace MechanicShop.Application.Common.Interfaces
+{
+    public interface ICachedQuery
+    {
+        string Key { get; }
+        string[] Tags { get; }
+        TimeSpan Expiration { get; }
+    }
+    public interface ICachedQuery<TResponse> : IRequest<TResponse>, ICachedQuery;
+}
