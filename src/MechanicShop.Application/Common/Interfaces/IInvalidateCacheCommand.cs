3a3d9c17d3ee38519b8a41903a1ca1edcebedeb6
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-17 12:00:49 +0300

    feat(Caching): add Genaric IInvalidateCacheCommand that implement IResult<>

--- src/MechanicShop.Application/Common/Interfaces/IInvalidateCacheCommand.cs
+++ src/MechanicShop.Application/Common/Interfaces/IInvalidateCacheCommand.cs
@@ -1,7 +1,10 @@
-﻿namespace MechanicShop.Application.Common.Interfaces
+﻿using MediatR;
+
+namespace MechanicShop.Application.Common.Interfaces
 {
     public interface IInvalidateCacheCommand
     {
         string[] Tags { get; }
     }
+    public interface IInvalidateCacheCommand<TResponse> : IRequest<TResponse>, IInvalidateCacheCommand;
 }
