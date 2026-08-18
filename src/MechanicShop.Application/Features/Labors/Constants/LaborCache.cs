20d722a38930ce34a1578527670149f60e81b98e
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-18 08:40:49 +0300

    feat(Application-Labors): implement GetLaborsQuery

---
+++ src/MechanicShop.Application/Features/Labors/Constants/LaborCache.cs
@@ -0,0 +1,9 @@
+﻿namespace MechanicShop.Application.Features.Labors.Constants
+{
+    public static class LaborCache
+    {
+        public const string Tag = "labors";
+
+        public const string AllKey = "labors:all";
+    }
+}
