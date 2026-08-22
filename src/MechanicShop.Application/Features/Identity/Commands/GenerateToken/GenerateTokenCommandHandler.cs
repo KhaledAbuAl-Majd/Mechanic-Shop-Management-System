5ebf4e6da6a6f8a1e3d1a9bbd18505b83f9495bd
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-22 11:16:38 +0300

    feat(Application-Identity): implement GenerateToken Command

---
+++ src/MechanicShop.Application/Features/Identity/Commands/GenerateToken/GenerateTokenCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MechanicShop.Application.Common.Interfaces;
+using MechanicShop.Application.Features.Identity.Dtos;
+using MechanicShop.Application.Features.Identity.Interfaces;
+using MechanicShop.Domain.Common.Results;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicShop.Application.Features.Identity.Commands.GenerateToken
+{
+    public sealed class GenerateTokenCommandHandler(
+        ILogger<GenerateTokenCommandHandler> logger,
+        IIdentityService identityService,
+        ITokenProvider tokenProvider) : IRequestHandler<GenerateTokenCommand, Result<TokenDto>>
+    {
+        private readonly ILogger<GenerateTokenCommandHandler> _logger = logger;
+        private readonly IIdentityService _identityService = identityService;
+        private readonly ITokenProvider _tokenProvider = tokenProvider;
+
+        public async Task<Result<TokenDto>> Handle(GenerateTokenCommand command, CancellationToken ct)
+        {
+            var userResponse = await _identityService.AuthenticateAsync(command.Email, command.Password, ct);
+
+            if (userResponse.IsError)
+            {
+                _logger.LogWarning("User Authentication error occured: {@Errors}", userResponse.Errors);
+
+                return userResponse.Errors;
+            }
+
+            var generateTokenResult = await _tokenProvider.GenerateJwtTokenAsync(userResponse.Value, ct);
+
+            if (generateTokenResult.IsError)
+            {
+                _logger.LogError("Generate token error occured: {@Errors}", generateTokenResult.Errors);
+
+                return generateTokenResult.Errors;
+            }
+
+            return generateTokenResult.Value;
+        }
+    }
+}
