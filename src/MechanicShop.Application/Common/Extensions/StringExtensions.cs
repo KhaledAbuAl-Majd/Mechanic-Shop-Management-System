d8574562858b8df50b94f6e0ff84081987c4eff2
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-22 17:04:05 +0300

    feat(Extensions): Create StringExtensions

---
+++ src/MechanicShop.Application/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace MechanicShop.Application.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string MaskEmail(this string email)
+        {
+            int atIndex = email.IndexOf('@');
+            if (atIndex <= 1)
+            {
+                return $"****{email.AsSpan(atIndex)}";
+            }
+
+            return email[0] + "****" + email[atIndex - 1] + email[atIndex..];
+        }
+    }
+}
