10c17d6f5b7649bfd2d482823001495805e25a29
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-22 12:17:41 +0300

    feat(Infrastructure): setup scaffolding for Infrastructure

---
+++ src/MechanicShop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuraion)
+    {
+        services.AddSingleton(TimeProvider.System);
+
+        return services;
+    }
+}
