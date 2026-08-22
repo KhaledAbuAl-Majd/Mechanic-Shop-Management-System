7bcf348ede1db7ce02bb77c5091d048112835cbf
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-22 18:11:28 +0300

    feat(Identity): Register Identity Services

--- src/MechanicShop.Infrastructure/DependencyInjection.cs
+++ src/MechanicShop.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using MechanicShop.Application.Common.Constants;
+using MechanicShop.Application.Common.Interfaces;
+using MechanicShop.Application.Common.Settings;
+using MechanicShop.Application.Features.Identity.Interfaces;
+using MechanicShop.Domain.Identity.Enums;
+using MechanicShop.Infrastructure.Identity;
+using MechanicShop.Infrastructure.Identity.Polices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +19,49 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuraion)
     {
         services.AddSingleton(TimeProvider.System);
+
+        var jwtSettings = configuraion.GetSection(JwtSettings.SectionName).Get<JwtSettings>();
+
+        ArgumentNullException.ThrowIfNull(jwtSettings);
+
+        services.TryAddSingleton(jwtSettings);
+
+
+        services.AddIdentity(jwtSettings);
+        return services;
+    }
+
+    private static IServiceCollection AddIdentity(this IServiceCollection services, JwtSettings jwtSettings)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            };
+        });
+
+        services.AddScoped<IAuthorizationHandler, LaborAssignedHandler>();
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy(AuthorizationPolicies.ManagerOnly, policy => policy.RequireRole(nameof(Role.Manager)))
+            .AddPolicy(AuthorizationPolicies.SelfScopedWorkOrderAccess, policy =>
+            policy.Requirements.Add(new LaborAssignedRequirement()));
+
+
+        services.TryAddScoped<IIdentityService, IdentityService>();
+        services.TryAddScoped<ITokenProvider, TokenProvider>();
 
         return services;
     }
