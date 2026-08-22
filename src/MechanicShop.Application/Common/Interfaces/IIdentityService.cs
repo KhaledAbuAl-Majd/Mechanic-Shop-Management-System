7c5ecd6cd8f6337adcaa5231d9714e30950f3067
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-22 10:42:19 +0300

    feat(Application-Identity): Create TokenDto, Constants, ITokenProvider and adding Cancellation Token at IIdentityService

--- src/MechanicShop.Application/Common/Interfaces/IIdentityService.cs
+++ src/MechanicShop.Application/Common/Interfaces/IIdentityService.cs
@@ -5,10 +5,10 @@ namespace MechanicShop.Application.Common.Interfaces
 {
     public interface IIdentityService
     {
-        Task<bool> IsInRoleAsync(string userId, string role);
-        Task<bool> AuthorizeAsync(string userId, string? policyName);
-        Task<Result<AppUserDto>> AuthenticateAsync(string email, string password);//login 
-        Task<Result<AppUserDto>> GetUserByIdAsync(string userId);
-        Task<string?> GetUserNameAsync(string userId);
+        Task<bool> IsInRoleAsync(string userId, string role, CancellationToken ct = default);
+        Task<bool> AuthorizeAsync(string userId, string? policyName, CancellationToken ct = default);
+        Task<Result<AppUserDto>> AuthenticateAsync(string email, string password, CancellationToken ct = default);//login 
+        Task<Result<AppUserDto>> GetUserByIdAsync(string userId, CancellationToken ct = default);
+        Task<string?> GetUserNameAsync(string userId, CancellationToken ct = default);
     }
 }
