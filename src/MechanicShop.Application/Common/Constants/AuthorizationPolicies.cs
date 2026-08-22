faea9ee3a3424cb220d16c6de8af9965c0cdc7a3
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-22 18:10:43 +0300

    feat(Application-Common-Constants): Create AuthorizationPolicies

---
+++ src/MechanicShop.Application/Common/Constants/AuthorizationPolicies.cs
@@ -0,0 +1,8 @@
+﻿namespace MechanicShop.Application.Common.Constants
+{
+    public static class AuthorizationPolicies
+    {
+        public const string ManagerOnly = "ManagerOnly";
+        public const string SelfScopedWorkOrderAccess = "SelfScopedWorkOrderAccess";
+    }
+}
