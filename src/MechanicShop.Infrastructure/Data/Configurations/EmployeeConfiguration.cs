b2839362197f662cfe00b107e453574c3e6a4266
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-25 11:47:37 +0300

    feat(data): add EF Core entity data configurations

---
+++ src/MechanicShop.Infrastructure/Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using MechanicShop.Domain.Employees;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MechanicShop.Infrastructure.Data.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employees");
+
+            builder.HasKey(b => b.Id).IsClustered(false);
+
+            builder.Property(b => b.FirstName).IsRequired().HasMaxLength(50);
+
+            builder.Property(b => b.LastName).IsRequired().HasMaxLength(50);
+
+            builder.Property(b => b.Role).HasConversion<string>().IsRequired().HasMaxLength(20);
+
+            builder.HasIndex(b => b.Role);
+
+            builder.HasIndex(b => new { b.FirstName, b.LastName });
+        }
+    }
+}
