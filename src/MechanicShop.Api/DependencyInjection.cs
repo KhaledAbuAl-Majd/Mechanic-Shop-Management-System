f98a095f79a45803fc41b5ca5ad05e167cbc656a
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-25 14:51:39 +0300

    chore(api): add initial project setup and dependency injection scaffolding

---
+++ src/MechanicShop.Api/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using MechanicShop.Application.Common.Settings;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+
+            services.AddSingleton(sp => sp.GetRequiredService<IOptions<AppSettings>>().Value);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCoreMiddlewares(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            app.UseExceptionHandler();
+
+            app.UseStatusCodePages();
+
+            return app;
+        }
+    }
+}
--- src/MechanicShop.Api/Program.cs
+++ src/MechanicShop.Api/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
+
+builder.Services
+    .AddPresentation(builder.Configuration)
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseCoreMiddlewares(builder.Configuration);
+
+app.MapControllers();
 
 app.Run();
