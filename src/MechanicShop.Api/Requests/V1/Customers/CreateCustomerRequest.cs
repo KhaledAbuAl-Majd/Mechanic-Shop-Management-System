9a6e0abb69622aeb2fb96f8f0c076dcd8181ade3
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-08-26 18:09:44 +0300

    edit(Customer): move request files

--- src/MechanicShop.Api/Requests/V1/Customers/CreateCustomerRequest.cs
+++ src/MechanicShop.Api/Requests/V1/Customers/CreateCustomerRequest.cs
@@ -1,10 +1,4 @@
 ﻿namespace MechanicShop.Api.Requests.V1.Customers
 {
     public sealed record CreateCustomerRequest(string Name, string PhoneNumber, string Email, List<CreateVehicleRequest> Vehicles);
-
-    public sealed record CreateVehicleRequest(string Make, string Model, int Year, string LicensePlate);
-
-    public sealed record UpdateCustomerRequest(string Name, string PhoneNumber, string Email, List<UpdateVehicleRequest> Vehicles);
-
-    public sealed record UpdateVehicleRequest(Guid VehicleId, string Make, string Model, int Year, string LicensePlate);
 }
